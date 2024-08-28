6f7776e91548b9106f40d86e52c566dce3df499d
corsini <christian.orsini@kairos-tech.it>
2024-08-28 12:53:30 +0200

    files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.0"
+DESCRIPTION = "Python package"
+LONG_DESCRIPTION = "Python package with a slightly longer description"
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="package-test",
+    version=VERSION,
+    author="Author Name",
+    author_email="<youremail@email.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=["python", "test package"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+)
---
+++ src/other/other.py
@@ -0,0 +1,4 @@
+class Other:
+
+  def __init__(self, name):
+    self.name = name
