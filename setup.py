5ca8d45f959b971a9786aa7d10e066c58bc4c499
corsini <christian.orsini@kairos-tech.it>
2024-08-28 15:59:28 +0200

    s

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     author_email="<youremail@email.com>",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
